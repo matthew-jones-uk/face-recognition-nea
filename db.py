e1d954650e9bc0d064b042fdc02e4daf7315a91a
Matthew Jones <matthewbtjones2000@gmail.com>
2019-02-03 17:48:41 +0000

    getFaceId route implemented and database handler finished

--- db.py
+++ db.py
@@ -14,8 +14,9 @@ def close_db():
         db.close()
 
 def init_db():
-    # check for tables and create them
-    ...
+    db = get_db()
+    with current_app.open_resource('schema.sql') as schema:
+        db.executescript(schema.read().decode('utf8'))
 
 def init_app(app):
     app.teardown_appcontext(close_db)
--- rest_server.py
+++ rest_server.py
@@ -3,6 +3,7 @@ import db
 from flask import Flask, jsonify
 from os.path import join
 from os import makedirs
+from uuid import uuid4
 
 app = Flask(__name__)
 app.config.from_mapping(
@@ -15,16 +16,48 @@ except OSError:
     pass
 
 db.init_app(app)
+db.init_db()
 
-@app.route('/getFaceId')
+'''
+Error Codes:
+0 - Success
+1 - Database Empty
+2 - Invalid UUID
+3 - Invalid vote
+4 - Invalid JSON data
+'''
+
+
+
+_temp_ids = dict()
+
+@app.route('/getFaceId', methods=['GET'])
 def get_face_id():
-    ...
+    created_id = False
+    while not created_id:
+        uuid = str(uuid4())
+        if uuid not in _temp_ids:
+            created_id = True
+    random_image_record = db.get_db().execute(
+        'SELECT * FROM table'
+        'WHERE id IN (SELECT id FROM table ORDER BY RANDOM() LIMIT 1)'
+        'AND active = 1'
+    ).fetchone()
+    if random_image_record is None:
+        response = jsonify(status=1, id="")
+        response.status_code = 404
+        return response
+    else:
+        _temp_ids[uuid] = random_image_record[0]
+        response = jsonify(status=0, id=uuid)
+        response.status_code = 200
+        return response
 
-@app.route('/getFaceImage')
+@app.route('/getFaceImage', methods=['POST'])
 def get_face_image():
     ...
 
-@app.route('/giveVote')
+@app.route('/giveVote', methods=['POST'])
 def give_vote():
     ...
 
