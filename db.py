f9d804095d5904535c1b23450da7a304fcb7f315
Matthew Jones <matthewbtjones2000@gmail.com>
2019-03-04 15:58:00 +0000

    Added docstrings to database handler

--- db.py
+++ db.py
@@ -2,6 +2,7 @@ import sqlite3
 from flask import current_app, g
 
 def get_db():
+    """Return database. Connect if not already defined."""
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config('DATABASE')
@@ -9,11 +10,15 @@ def get_db():
     return g.db
 
 def close_db():
-    db = g.pop('db',None)
+    """Function to close database on Flask application shutdown."""
+    db = g.pop('db', None)
     if db is not None:
         db.close()
 
 def init_db():
+    """Run SQL schema on database init, if database is not available
+    this will create it
+    """
     db = get_db()
     with current_app.open_resource('schema.sql') as schema:
         db.executescript(schema.read().decode('utf8'))
