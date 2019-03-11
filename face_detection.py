cbdb31b6cf139ed60ab51bed5a356a0ccf724de5
Matthew Jones <matthewbtjones2000@gmail.com>
2019-03-11 22:17:59 +0000

    Fixed formatting to abide to PEP guidelines

--- face_detection.py
+++ face_detection.py
@@ -1,8 +1,7 @@
-from multiprocessing import Pool, Queue
+from multiprocessing import Pool
 from functools import partial
 from os.path import join
 from os import listdir
-import logging
 import warnings
 #from skimage.feature import hog
 from skimage.transform import rescale, resize
@@ -39,7 +38,8 @@ class Face():
         Returns:
             face_image (numpy.array): Cropped image of face in numpy array form.
         '''
-        self.face_image = image[self.top_left[0]:self.bottom_right[0],self.top_left[1]:self.bottom_right[1]]
+        self.face_image = image[self.top_left[0]:self.bottom_right[0],
+                                self.top_left[1]:self.bottom_right[1]]
         return self.face_image
 
 class DetectionOptions():
@@ -91,7 +91,8 @@ def load_image(path, as_gray=True):
     '''
     return imread(path, as_gray=as_gray)
 
-def _sliding_window(image, model, scale, hog_options=HOGOptions(), detection_options=DetectionOptions()):
+def _sliding_window(image, model, scale, hog_options=HOGOptions(),
+                    detection_options=DetectionOptions()):
     '''A sliding window worker method to find faces using image pyramid scales.
     Args:
         image (list): A black and white list represented image to be scanned.
@@ -116,15 +117,14 @@ def _sliding_window(image, model, scale, hog_options=HOGOptions(), detection_opt
             # Crop the desired window from the image.
             window = rescaled_image[row_start:row_end, column_start:column_end]
             # Calculate the Histogram of Orientate Gradients for the desired window.
-            window_hog = hog(window, orientations=hog_options.orientations,
-                             pixels_per_cell=hog_options.pixels_per_cell,
-                             cells_per_block=hog_options.cells_per_block)
+            window_hog = hog(window, options=hog_options)
             # Calculate the probability of the window containing a face.
             model_probability = model.predict_proba([window_hog])[:, 1][0]
             if model_probability >= detection_options.accept_threshold:
                 # Scale the window coordinates to the full size image
-                found_faces.append(Face((int(row_start*scale), int(column_start*scale)), (int(row_end*scale),
-                                   int(column_end*scale)), model_probability))
+                found_faces.append(Face((int(row_start*scale), int(column_start*scale)),
+                                        (int(row_end*scale), int(column_end*scale)),
+                                        model_probability))
     return found_faces
 
 def find_all_face_boxes(image, complete_model, detection_options=DetectionOptions()):
@@ -139,11 +139,12 @@ def find_all_face_boxes(image, complete_model, detection_options=DetectionOption
     '''
     model = complete_model.svm_model
     hog_options = complete_model.hog_options
-    # Raise an IndexError if file too small. 
+    # Raise an IndexError if file too small
     if image.shape[0] < hog_options.window_size[0] or image.shape[1] < hog_options.window_size[1]:
         print('Image is too small for set window size')
         raise IndexError
-    # Calculate the maximum factor that the window can be multiplied by according to the size of the image.
+    # Calculate the maximum factor that the window can be multiplied by according to the size
+    # of the image.
     if image.shape[0] < image.shape[1]:
         maximum_factor = image.shape[0] / hog_options.window_size[0]
     else:
@@ -155,10 +156,12 @@ def find_all_face_boxes(image, complete_model, detection_options=DetectionOption
     while scale_factor <= maximum_factor:
         scales.append(scale_factor)
         scale_factor += detection_options.factor_difference
-    # No maximum processes is defined so it will default to the number of CPU cores.
+    # No maximum processes is defined so it will default to the number of CPU cores
     pool = Pool()
-    # Creates a partial object so that the pool map can properly pass arguments to the sliding window function.
-    function = partial(_sliding_window, image, model, hog_options=hog_options, detection_options=detection_options)
+    # Creates a partial object so that the pool map can properly pass arguments to the sliding
+    # window function.
+    function = partial(_sliding_window, image, model, hog_options=hog_options,
+                       detection_options=detection_options)
     # Uses the worker processes to run the sliding window function.
     possible_faces = pool.map(function, scales)
     pool.close()
@@ -171,16 +174,18 @@ def generate_hog_data(image, hog_options=HOGOptions()):
     '''Generate Histogram of Oriented Gradients features from given image.
     Args:
         image (numpy.array): Image to calculate features from as a numpy array.
-        hog_options (HOGOptions, optional): Defaults to HOGOptions(). Configuration for HOG algorithm.
+        hog_options (HOGOptions, optional): Defaults to HOGOptions(). Configuration for HOG
+                                            algorithm.
     Returns:
         hog_image (numpy.array): Features of HOG data extracted from image.
     '''
-    # Check if image is correctly sized, if not resize. This may cause images to be distorted and is not prefered.
+    # Check if image is correctly sized, if not resize. This may cause images to be distorted
+    # and is not prefered.
     if image.shape != hog_options.window_size:
         print('Resizing this could potentially lead to bad data')
-        image = resize(image,hog_options.window_size)
+        image = resize(image, hog_options.window_size)
     # Calculate the Histogram of Orientate Gradients for the desired window with defaults.
-    hog_image = hog(image)
+    hog_image = hog(image, options=hog_options)
     return hog_image
 
 def generate_hog_data_from_dir(folder_path, hog_options=HOGOptions(), limit=None):
@@ -211,43 +216,43 @@ def generate_hog_data_from_dir(folder_path, hog_options=HOGOptions(), limit=None
             print('Failed to load '+image_name)
     return hog_data
 
-def _calculate_equalise(x, y):
+def _calculate_equalise(x_size, y_size):
     '''Calculate the size the datasets should be cropped to.
     Args:
-        x (integer): First dataset size.
-        y (integer): Second dataset size.
+        x_size (integer): First dataset size.
+        y_size (integer): Second dataset size.
     Returns:
         integer: Size datasets should be cropped to.
     '''
-    return min(x, y)
+    return min(x_size, y_size)
 
-def split_training_data(x, y, test_percentage=0.2, equalise=True):
+def split_training_data(x_data, y_data, test_percentage=0.2, equalise=True):
     '''This splits data into training and testing sets using a given percentage.
     Args:
-        x (list): The first set of data.
-        y (list): The second set of data.
+        x_data (list): The first set of data.
+        y_data (list): The second set of data.
         test_percentage (float, optional): Defaults to 0.2. Percentage to be used as testing data.
         equalise (bool, optional): Defaults to True. If datasets should be equal sizes.
     Returns:
-        x (list): First dataset for training.
-        y (list): Second dataset for training.
+        x_train (list): First dataset for training.
+        y_train (list): Second dataset for training.
         x_test (list): First dataset for testing.
         y_test (list): Second dataset for testing.
     '''
     # If equalisation enabled and possible with data, equalise
-    if equalise and len(x) != len(y):
-        max_value = _calculate_equalise(len(x), len(y))
-        x = x[:max_value]
-        y = y[:max_value]
+    if equalise and len(x_data) != len(y_data):
+        max_value = _calculate_equalise(len(x_data), len(y_data))
+        x_data = x_data[:max_value]
+        y_data = y_data[:max_value]
     # Calculate testing sizes for the two datasets.
-    x_test_size = int(len(x)*test_percentage)
-    y_test_size = int(len(y)*test_percentage)
+    x_test_size = int(len(x_data)*test_percentage)
+    y_test_size = int(len(y_data)*test_percentage)
     # Sort the datasets into training and testing.
-    x_test = x[:x_test_size]
-    y_test = y[:y_test_size]
-    x = x[x_test_size:]
-    y = y[y_test_size:]
-    return x, y, x_test, y_test
+    x_test = x_data[:x_test_size]
+    y_test = y_data[:y_test_size]
+    x_train = x_data[x_test_size:]
+    y_train = y_data[y_test_size:]
+    return x_train, y_train, x_test, y_test
 
 def premade_train(positive_train, negative_train, positive_test, negative_test):
     """Similar to the train() function, however this is for when users have
@@ -261,18 +266,18 @@ def premade_train(positive_train, negative_train, positive_test, negative_test):
         classifier (LinearSVM): just the completed SVM model.
         score (float): the accuracy of the model.
     """
-    x = list()
-    y = list()
+    x_train = list()
+    y_train = list()
     x_test = list()
     y_test = list()
     # Assign positive data with label of 1 to mark it is positive
     for data in positive_train:
-        x.append(data)
-        y.append(1)
+        x_train.append(data)
+        y_train.append(1)
     # Assign negative data with label of 0 to mark it is negative
     for data in negative_train:
-        x.append(data)
-        y.append(0)
+        x_train.append(data)
+        y_train.append(0)
     # Assign test data similarly
     for data in positive_test:
         x_test.append(data)
@@ -283,11 +288,11 @@ def premade_train(positive_train, negative_train, positive_test, negative_test):
     # Briefly calculate best values for training
     parameter_grid = {'C': [0.001, 0.01, 0.1, 1, 10]}
     grid_search = GridSearchCV(LinearSVC(), parameter_grid)
-    grid_search.fit(x, y)
+    grid_search.fit(x_train, y_train)
     # Calculate SVM using all data given
     svm = grid_search.best_estimator_
     classifier = CalibratedClassifierCV(svm)
-    classifier.fit(x, y)
+    classifier.fit(x_train, y_train)
     # Multiply to a percentage and round to 2 decimal places
     score = round(classifier.score(x_test, y_test) * 100)
     return classifier, score
@@ -308,10 +313,12 @@ def train(positive_path, negative_path, hog_options=HOGOptions(), train_options=
     positive_hog = generate_hog_data_from_dir(positive_path, hog_options=hog_options)
     negative_hog = generate_hog_data_from_dir(negative_path, hog_options=hog_options)
     # Split training data for testing and training
-    pos_train, neg_train, pos_test, neg_test = split_training_data(positive_hog, negative_hog,
-        test_percentage=train_options.testing_proportion, equalise=train_options.equalise)
+    test_prop = train_options.testing_proportion
+    p_train, n_train, p_test, n_test = split_training_data(positive_hog, negative_hog,
+                                                           test_percentage=test_prop,
+                                                           equalise=train_options.equalise)
     # Train SVM
-    svm_model, score = premade_train(pos_train, neg_train, pos_test, neg_test)
+    svm_model, score = premade_train(p_train, n_train, p_test, n_test)
     # Contain SVM model and HOG configuration inside a Model object
     model = Model(svm_model, round(score), hog_options)
     return model
