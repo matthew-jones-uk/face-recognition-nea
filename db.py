e3d3beac7f95c2e5686a07cdceaf7c3083b8ddac
Matthew Jones <matthewbtjones2000@gmail.com>
2019-03-18 15:42:31 +0000

    Added thread-safe way of handling database with locks

--- db.py
+++ db.py
@@ -1,27 +1,18 @@
 import sqlite3
-from time import time
-from uuid import uuid4
+from threading import Lock
 
-def get_db(database_path):
-    """Return database"""
-    connection = sqlite3.connect(database_path)
-    return connection
-
-def init_db(database_path):
-    """Run SQL schema on database init, if database is not available
-    this will create it
-    """
-    db = sqlite3.connect(database_path)
-    db.executescript("""
-       CREATE TABLE IF NOT EXISTS images (
-                id text UNIQUE PRIMARY KEY NOT NULL, 
-                filename text UNIQUE NOT NULL,
-                start_date integer,
-                probability real,
-                positive_votes integer DEFAULT 0,
-                negative_votes integer DEFAULT 0,
-                needed_votes integer NOT NULL,
-                active integer DEFAULT 1
-        )     
-    """)
-    db.close()
+class ThreadHandler():
+    def __init__(self, database_path):
+        self.lock = Lock()
+        self.connection = sqlite3.connect(database_path, check_same_thread=False)
+        self.cursor = None
+    def __enter__(self):
+        self.lock.acquire()
+        self.cursor = self.connection.cursor()
+        return self
+    def __exit__(self, type, value, traceback):
+        self.connection.commit()
+        if self.cursor is not None:
+            self.cursor.close()
+            self.cursor = None
+        self.lock.release()
--- rest_server.py
+++ rest_server.py
@@ -19,6 +19,8 @@ DETECTOR_TIMEOUT = 60
 
 app = Flask(__name__)
 
+db_handler = db.ThreadHandler(join(INSTANCE, DATABASE_FILENAME))
+
 '''
 Error Codes:
 0 - Success
@@ -32,14 +34,13 @@ _temp_ids = dict()
 
 @app.route('/getFaceId', methods=['GET'])
 def get_face_id():
-    """ Function for the route to get the face id in json format"""
-    connection = db.get_db(join(INSTANCE, DATABASE_FILENAME))
-    random_image_record = connection.execute(
-        'SELECT * FROM images WHERE active = 1 ORDER BY RANDOM() LIMIT 1'
-    ).fetchone()
-    connection.close()
+    ''' Function for the route to get the face id in json format'''
+    with db_handler:
+        random_image_record = db_handler.cursor.execute(
+            'SELECT * FROM images WHERE active = 1 ORDER BY RANDOM() LIMIT 1'
+        ).fetchone()
     if random_image_record is None:
-        response = jsonify(status=1, id="")
+        response = jsonify(status=1, id='')
         response.status_code = 404
         return response
     else:
@@ -49,19 +50,18 @@ def get_face_id():
 
 @app.route('/getFaceImage', methods=['GET'])
 def get_face_image():
-    """ Function for the route to get the face image in base64 json when given id"""
+    ''' Function for the route to get the face image in base64 json when given id'''
     try:
         given_id = request.headers.get('id')
     except KeyError:
         # if there's no id, return status 400 bad request with invalid json data json status code
-        response = jsonify(status=4, id="")
+        response = jsonify(status=4, id='')
         response.status_code = 400
         return response
-    connection = db.get_db(join(INSTANCE, DATABASE_FILENAME))
-    record = connection.execute(
-        'SELECT * FROM images WHERE id = "{}"'.format(given_id)
-    ).fetchone()
-    connection.close()
+    with db_handler:
+        record = db_handler.cursor.execute(
+            'SELECT * FROM images WHERE id = "{}"'.format(given_id)
+        ).fetchone()
     if record is None:
         # if no record is found, return status 404 not found with invalid id json status code
         response = jsonify(status=2, id=given_id)
@@ -94,7 +94,7 @@ def get_face_image():
 
 @app.route('/giveVote', methods=['POST'])
 def give_vote():
-    """ Function for the route to give a vote on whether or not a face """
+    ''' Function for the route to give a vote on whether or not a face '''
     json = request.get_json(silent=True)
     if json is None:
         # check for json data, if not found then return status 400 bad request with
@@ -110,43 +110,41 @@ def give_vote():
         return response
     else:
         given_id, vote = json['id'], json['vote']
-        connection = db.get_db(join(INSTANCE, DATABASE_FILENAME))
         # check if id is valid in database
-        record = connection.execute(
-            'SELECT * FROM images WHERE id = "{}"'.format(given_id)
-        ).fetchone()
+        with db_handler:
+            record = db_handler.cursor.execute(
+                'SELECT * FROM images WHERE id = "{}"'.format(given_id)
+            ).fetchone()
         if record is None:
-            connection.close()
             # if id is invalid return status 400 not found with invalid id json status code
             response = jsonify(status=2, id=given_id)
             response.status_code = 404
             return response
         # consider the vote according to json data and add to database
         if vote:
-            connection.execute("""
-                UPDATE images
-                SET positive_votes = positive_votes + 1
-                WHERE id = "{}"
-            """.format(given_id))
-            connection.commit()
+            with db_handler:
+                db_handler.cursor.execute('''
+                    UPDATE images
+                    SET positive_votes = positive_votes + 1
+                    WHERE id = "{}"
+                '''.format(given_id))
         else:
-            connection.execute("""
-                UPDATE images
-                SET negative_votes = negative_votes + 1
-                WHERE id = "{}"
-            """.format(given_id))
-            connection.commit()
-        connection.close()
+            with db_handler:
+                db_handler.cursor.execute('''
+                    UPDATE images
+                    SET negative_votes = negative_votes + 1
+                    WHERE id = "{}"
+                '''.format(given_id))
         # return status code 200 OK and success json status code
         response = jsonify(status=0, id=given_id)
         response.status_code = 200
         return response
 
 def get_model():
-    """This function retrieves the latest saved version of the detection model based on filenames.
+    '''This function retrieves the latest saved version of the detection model based on filenames.
     Returns:
         face_detection.Model: Detection model
-    """
+    '''
     # retrieve all model files and calculate latest based on number after root name
     all_files = listdir(INSTANCE)
     model_files = dict()
@@ -166,12 +164,12 @@ def get_model():
     return pickle.load(join(INSTANCE, model))
 
 def detect_faces(image):
-    """This founction uses the face_detection module to find any faces in an image
+    '''This founction uses the face_detection module to find any faces in an image
     Args:
         image (Image): Skimage image array format
     Returns:
         list: Returns a list of face_detection.Face objects
-    """
+    '''
     model = get_model()
     found_faces = face_detection.find_all_face_boxes(image, model)
     for face in found_faces:
@@ -179,9 +177,9 @@ def detect_faces(image):
     return found_faces
 
 def new_image_detector():
-    """Checks the testing image directory for any new images then processes and adds to database.
+    '''Checks the testing image directory for any new images then processes and adds to database.
        Should be run in own process due to CPU and IO heavy and blocking nature.
-    """
+    '''
     while True:
         # get all files in directory
         files = listdir(join(INSTANCE, DATABASE_TESTING_IMAGES_DIRECTORY))
@@ -192,38 +190,38 @@ def new_image_detector():
             remove(join(INSTANCE, DATABASE_TESTING_IMAGES_DIRECTORY, image_file))
         # for any detected face generate unique id, save file and add to database
         for face in faces:
-            connection = db.get_db(join(INSTANCE, DATABASE_FILENAME))
             checking = True
             while checking:
                 unique_id = uuid4()
-                record = connection.execute('SELECT * FROM images WHERE id = {}'.format(unique_id)
-                                                                                        ).fetchone()
+                with db_handler:
+                    record = db_handler.cursor.execute('''
+                                SELECT * FROM images WHERE id = "{}"'''.format(unique_id)).fetchone()
                 if not record:
                     checking = False
             filename = unique_id+'.png'
             # calculate needed votes out of 10
             needed_votes = 10 - round(face.probability*100, -1)/10
-            connection.execute("""
-                INSERT INTO images (
-                    id,
-                    filename,
-                    start_date,
-                    probability,
-                    needed_votes
-                ) VALUES (
-                    {},
-                    {},
-                    {},
-                    {},
-                    {}
-                )
-            """.format(unique_id, filename, time(), face.probability, needed_votes))
-            connection.close()
+            with db_handler:
+                db_handler.cursor.execute('''
+                    INSERT INTO images (
+                        id,
+                        filename,
+                        start_date,
+                        probability,
+                        needed_votes
+                    ) VALUES (
+                        {},
+                        {},
+                        {},
+                        {},
+                        {}
+                    )
+                '''.format(unique_id, filename, time(), face.probability, needed_votes))
             imsave(filename, face.face_image)
         sleep(DETECTOR_TIMEOUT)
 
 def run():
-    """ Main application function """
+    ''' Main application function '''
     # this will check if required directories exist and if they don't, create them
     if not isdir(INSTANCE):
         makedirs(INSTANCE)
@@ -232,8 +230,20 @@ def run():
     if not isdir(join(INSTANCE, DATABASE_TESTING_IMAGES_DIRECTORY)):
         makedirs(join(INSTANCE, DATABASE_TESTING_IMAGES_DIRECTORY))
     # creates database if not already made
-    db.init_db(join(INSTANCE, DATABASE_FILENAME))
-    app.run(debug=True, host="0.0.0.0")
+    with db_handler:
+        db_handler.cursor.executescript('''
+        CREATE TABLE IF NOT EXISTS images (
+                    id text UNIQUE PRIMARY KEY NOT NULL, 
+                    filename text UNIQUE NOT NULL,
+                    start_date integer,
+                    probability real,
+                    positive_votes integer DEFAULT 0,
+                    negative_votes integer DEFAULT 0,
+                    needed_votes integer NOT NULL,
+                    active integer DEFAULT 1
+            )     
+        ''')
+    app.run(debug=True, host='0.0.0.0')
 
 if __name__ == '__main__':
     run()
